572c325a2b0e3df9e53450fa6d6d75a75ec30a41
UmidYul <yumid255@gmail.com>
2023-08-05 19:14:41 +0300

    2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000
 app.use(express.static(__dirname + "/public/"))
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.htm")
+    res.sendFile(__dirname + "/views/index.html")
 })
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/views/about.htm")
