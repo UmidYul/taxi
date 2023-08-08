6febfa5e5a546deb6ff25b2fa83608dd623bdb72
UmidYul <yumid255@gmail.com>
2023-08-08 17:56:44 +0500

    we

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express()
-const port = 7000
+const port = 4000
 app.use(express.static(__dirname + "/public/"))
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html",console.log("200"))
+    res.sendFile(__dirname + "/views/index.html")
 })
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/views/about.htm")
