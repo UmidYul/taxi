016f592fa0ef835d265fdd3dea04dab643211f15
UmidYul <yumid255@gmail.com>
2023-08-09 03:58:57 +0500

    zxc

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 import express from "express"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import bodyParser from "body-parser";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express()
 const port = 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public/"))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "./views/index.html")
 })
 app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "./views/about.htm")
+    res.sendFile(__dirname + "./views/about.html")
 })
 // app.get("/contacts", (req, res) => {
 //     res.sendFile(__dirname + "/views/contacts.htm")
