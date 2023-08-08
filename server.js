6ace9db5af52bb32bc056e8bcac7751594f71817
UmidYul <yumid255@gmail.com>
2023-08-08 17:10:08 +0500

    13

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express()
-const port = 3000
+const port = 7000
 app.use(express.static(__dirname + "/public/"))
 
 app.get("/", (req, res) => {
