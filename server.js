f4a3ba9792ca07b664a16f87e8f3874469e725eb
UmidYul <yumid255@gmail.com>
2023-08-09 05:23:52 +0500

    dsfsd

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 import express from "express"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import bodyParser from "body-parser";
-import { Telegraf } from "telegraf"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const bot = new Telegraf("5648239366:AAE-VuCYZDTQiWawrFGS6JBNujvyk0f2HfQ")
 const app = express()
 const port = 3000
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public/"))
 
 app.get('/', function (req, res) {
